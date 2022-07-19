e752c741020ec3286744f891f5452c8de0b58045
hans bousquet lafond <hans.bl@icloud.com>
2022-07-19 19:57:39 +0100

    device set up

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,4 @@
 Rails.application.routes.draw do
-  # get 'purchases/new'
-  # get 'purchases/create'
-  # get 'ipods/index'
-  # get 'ipods/show'
-  # get 'ipods/new'
-  # get 'ipods/create'
-  # get 'ipods/update'
-  # get 'ipods/edit'
-  # get 'users/new'
-  # get 'users/create'
-  # get 'users/show'
   devise_for :users
   root to: 'ipods#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
