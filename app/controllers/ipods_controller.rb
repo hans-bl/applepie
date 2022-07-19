41c756dfd63f7487af924c393bc5cd1f64d5afae
hans bousquet lafond <hans.bl@icloud.com>
2022-07-19 21:08:06 +0100

    starting ipod crud actions

--- app/controllers/ipods_controller.rb
+++ app/controllers/ipods_controller.rb
@@ -6,14 +6,24 @@ class IpodsController < ApplicationController
   end
 
   def new
+    @ipod = Ipod.new
   end
 
   def create
+    @ipod = Ipod.new(ipod_params)
+    @ipod.save
+    redirect_to ipod_path(@ipod)
   end
 
   def update
   end
 
   def edit
+  end
+
+  private
+
+  def ipod_params
+    params.require(:ipod).permit(:name, :description, :model, :color, :state, :ipod_picture_url, :price, :capacity)
   end
 end
