4fa5268b19556f25da29e8415b4ad11b8bd9fc45
fernanda freitas <idea.fernanda@gmail.com>
2022-07-23 12:07:57 +0100

    creation of a purchase

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,7 +1,24 @@
 class PurchasesController < ApplicationController
   def new
+    @ipod = Ipod.find(params[:ipod_id])
+    @purchase = Purchase.new
   end
 
   def create
+    @purchase = Purchase.new(purchase_params)
+    @ipod = Ipod.find(params[:ipod_id])
+    @purchase.ipod = @ipod
+    @purchase.user = current_user
+    if @purchase.save
+      redirect_to user_path(current_user)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.require(:purchase).permit(:status, :delivery_address, :phone_number, :message)
   end
 end
