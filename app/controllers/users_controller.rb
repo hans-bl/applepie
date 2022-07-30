0676ab846777db1b1249d4a515b922c9d86d3f25
fernanda freitas <idea.fernanda@gmail.com>
2022-07-30 11:40:39 +0100

    profile page with selling ipods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,9 @@ class UsersController < ApplicationController
   end
 
   def show
+    #ipods that current_user is selling
+    @selling = current_user.ipods
+    #ipods that current user has bought
+    @purchases = current_user.purchases
   end
 end
